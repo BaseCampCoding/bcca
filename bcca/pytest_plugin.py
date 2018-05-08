c56fa7bdc32d0f7c67b1a3d91bc23e246d3d027b
Nate Clark <natec425@gmail.com>
2018-05-08 09:43:30 -0500

    Add with_inputs
    with_inputs lets the students test their code that expects
    input from the command line by providing expected return values.

--- bcca/pytest_plugin.py
+++ bcca/pytest_plugin.py
@@ -1,5 +1,6 @@
 from _pytest.assertion.util import assertrepr_compare
 from io import StringIO
+import sys
 from unittest import mock
 
 
@@ -13,6 +14,18 @@ class FakeStringIO(StringIO):
     def __repr__(self):
         return repr(self.getvalue().strip())
 
+class FakeStdIn:
+    def __init__(self, responses):
+        self.responses = responses
+        self.idx = 0
+
+    def readline(self):
+        if self.idx >= len(self.responses):
+            raise AssertionError('No more input provided!')
+        else:
+            self.idx += 1
+            sys.stdout.write(self.responses[self.idx - 1] + '\n')
+            return self.responses[self.idx - 1]
 
 def pytest_assertrepr_compare(config, op, left, right):
     if isinstance(left, FakeStringIO) and isinstance(right, str) and op == '==':
