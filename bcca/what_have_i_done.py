0515e5298414642db240b688bee1d68a3c30cb44
Nate Clark <natec425@gmail.com>
2018-05-09 13:25:10 -0500

    Add --num param to what_have_i_done
    I want the fish_greeting for the students to be a quick refresher
    on their recent commands. So, I thought what_have_i_done was
    almost what I needed. I just needed to add a way to limit how far
    back it would look.
    
    So this is a larger commit than I would like, but it ended up
    hitting a couple things.
    
    1) Looking at ~/.bash_history was outdated, so I changed it to
       calling out to `fish -c "history"` instead. At first I thought
       this was because of the shell buffering writes to the file, but
       it might also have to do with `fish` probably not writing to
       ~/.bash_history =). Either way, fish will be installed on the
       machines and I'm happy enough with `fish -c "history"`
    
    2) Some refactoring to keep my sanity when trying to add the
       --num parameter.
    
    3) Add click as a dependency for cli argument parsing.

--- bcca/what_have_i_done.py
+++ bcca/what_have_i_done.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 from itertools import groupby
+from subprocess import check_output
+import click
 
 
 def program(line):
@@ -14,19 +16,50 @@ def take(n, iter):
     for _, e in zip(range(n), iter):
         yield e
 
+def validate_num_commands(ctx, param, value):
+    if value is None or  value > 0:
+        return value
+    else:
+        raise click.BadParameter("num must be a positive number")
 
-def main():
-    bash_history = Path.home() / '.bash_history'
-    lines = bash_history.read_text().split('\n')
-    non_empty_lines = sorted(l.strip() for l in lines if l.strip())
-    lines_by_program = [(program, list(lines))
-                        for program, lines in groupby(non_empty_lines, program)]
+def get_history():
+    output = check_output(['fish', '-c', 'history']).decode()
+    commands = output.split('\n')
+    return [c.strip() for c in commands if c.strip()]
 
-    for prog, lines in sorted(lines_by_program, key=number_of_calls):
-        print(prog)
-        for line in take(3, set(lines)):
-            print(f'    {line}')
+def recent_commands(history, num):
+    seen_programs = set()
+    commands = []
+    for command in history:
+        program, *rest = command.split()
+        seen_programs.add(program)
+        if len(seen_programs) > num:
+            break
+        else:
+            commands.append(command)
+    return commands
 
+def group_by_command(commands):
+    return [
+        (program, list(lines))
+        for program, lines
+        in groupby(sorted(commands), program)
+    ]
+
+
+@click.command()
+@click.option('--num', type=int, callback=validate_num_commands)
+def main(num):
+    history = get_history()
+    if num is not None:
+        commands = recent_commands(history, num)
+    else:
+        commands = history
+    grouped = group_by_command(commands)
+    for command, examples in sorted(grouped, key=number_of_calls):
+        print(command)
+        for example in take(3, set(examples)):
+            print('    ' + example)
 
 if __name__ == '__main__':
     main()
