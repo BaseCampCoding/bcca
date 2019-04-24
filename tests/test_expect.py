8045a8adeb8a0441364edf64602d9d306888991f
Nate Clark <natec425@gmail.com>
2019-04-24 13:15:33 -0500

    autoformat

--- tests/test_expect.py
+++ tests/test_expect.py
@@ -19,72 +19,63 @@ def test_expect_add_can_fail():
 
 
 def test_expect_greet_to_print_greeting():
-    @expect(name='nate', to_print='hello nate')
+    @expect(name="nate", to_print="hello nate")
     def greet(name):
-        print('hello', name)
+        print("hello", name)
 
     assert passes_expectations(greet)
 
 
 def test_expect_greet_can_fail():
-    @expect(name='evil nate', to_print='hello nate')
+    @expect(name="evil nate", to_print="hello nate")
     def greet(name):
-        print('hello', name)
+        print("hello", name)
 
     assert not passes_expectations(greet)
 
 
 def test_printing_multiline():
-    @expect(to_print='''hello
-world''')
+    @expect(
+        to_print="""hello
+world"""
+    )
     def hello_world():
-        print('hello')
-        print('world')
+        print("hello")
+        print("world")
 
     assert passes_expectations(hello_world)
 
 
 def test_expectation_can_assume_inputs():
-    @expect(with_inputs=['nate', 'clark'], to_return='nate clark')
+    @expect(with_inputs=["nate", "clark"], to_return="nate clark")
     def test_inputs():
-        return input() + ' ' + input()
+        return input() + " " + input()
 
     assert passes_expectations(test_inputs)
 
 
 def test_expectations_can_assume_inputs_and_expect_output():
     @expect(
-        with_inputs=['Nate'],
-        to_print='''
+        with_inputs=["Nate"],
+        to_print="""
 What is your name? Nate
 Hello, Nate
-''')
+""",
+    )
     def test_both():
-        name = input('What is your name? ')
-        print('Hello,', name)
+        name = input("What is your name? ")
+        print("Hello,", name)
 
     assert passes_expectations(test_both)
 
 
 def test_expectations_can_stub_out_a_file():
-    @expect(
-        with_fake_files={'foo.txt': 'hello world'}, to_return='hello world')
+    @expect(with_fake_files={"foo.txt": "hello world"}, to_return="hello world")
     def test_simple_read():
-        return open('foo.txt').read()
+        return open("foo.txt").read()
 
     assert passes_expectations(test_simple_read)
 
-
-# @fake_file({'foo.txt': 'hello world'})
-# def test_open_other_file_errors():
-#     with pytest.raises(ValueError):
-#         assert open('bar.txt')
-
-# @fake_file({'foo.txt': 'hello world', 'bar.txt': 'game over'})
-# def test_two_files():
-#     assert open('foo.txt').read() == 'hello world'
-
-#     assert open('bar.txt').read() == 'game over'
 
 # @fake_file({'foo.txt': 'hello\nworld'})
 # def test_readline_works():
